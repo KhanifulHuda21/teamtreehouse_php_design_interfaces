cf94f7b834369c5081632dd050fd757651385d77
KhanifulHuda21 <70567730+KhanifulHuda21@users.noreply.github.com>
2020-10-15 13:43:39 +0700

    project finish

---
+++ php_design_interfaces/views/footer.php
@@ -0,0 +1,9 @@
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="text-center text-muted">by Khaniful Huda at Treehouse &copy;2020</p>
+    </div>
+</footer>
+</body>
+</html>
